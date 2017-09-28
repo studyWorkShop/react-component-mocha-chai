7b51cedb5fe62d33bb9c3f04898684f58528f224
ruyu You <ryyou@thoughtworks.com>
2017-09-28 15:17:44 +0800

    change router edition

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>
+  , document.getElementById('root'));

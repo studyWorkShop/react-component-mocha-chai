407157d6bb80efc075b321c607fb0cd990b5620b
ruyu You <ryyou@thoughtworks.com>
2017-09-29 15:49:11 +0800

    change file structure and add test

--- src/components/index.js
+++ src/components/index.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import BoxModel from './BoxModelComponent';
 import Flex from './FlexComponent';
+import App from './App';
 
-const App = () => (
+ReactDOM.render(
   <BrowserRouter>
     <div>
-      <Link to="/box-model">
-        <button>
-          盒模型
-        </button>
-      </Link>
-      <Link to="/flex">
-        <button>
-          Flex
-        </button>
-      </Link>
-
+      <App/>
       <Route exact path="/" component={BoxModel}/>
       <Route path="/box-model" component={BoxModel}/>
       <Route path="/flex" component={Flex}/>
     </div>
   </BrowserRouter>
-);
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+  , document.getElementById('root'));

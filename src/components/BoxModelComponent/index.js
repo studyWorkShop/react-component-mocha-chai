19d9f782a30fefc92bfea4e6ed7248495a949cbc
ruyu You <ryyou@thoughtworks.com>
2017-09-30 10:42:27 +0800

    change box-model and flex struction

--- src/components/BoxModelComponent/index.js
+++ src/components/BoxModelComponent/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const BoxModel = () => (
-  <div>
-    BoxModel
-  </div>
-);
+const boxModel = ['盒模型', 'Display', 'Box-sizing'];
+
+class BoxModel extends React.Component {
+  render() {
+    return (
+      <ul>
+        {
+          boxModel.map((item, index) => {
+            return (
+              <li key={index}>{item}</li>
+            );
+          })
+        }
+      </ul>
+    );
+  }
+}
 
 export default BoxModel;
--- src/components/FlexComponent/index.js
+++ src/components/FlexComponent/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const Flex = () => (
-  <div>
-    Flex
-  </div>
-);
+const flexItems = ['Flex1', 'Flex2', 'Flex3'];
+
+class Flex extends React.Component {
+  render() {
+    return (
+      <ul>
+        {
+          flexItems.map((item, index) => {
+            return (
+              <li key={index}>{item}</li>
+            );
+          })
+        }
+      </ul>
+    )
+  }
+}
 
 export default Flex;

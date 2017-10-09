b03e4b80ed8df4f1d9a00abc2b52236fb3efa300
ruyu You <ryyou@thoughtworks.com>
2017-10-09 17:30:05 +0800

    Refactor: change display component to stateless component

--- src/components/BoxModelComponent/Display.js
+++ src/components/BoxModelComponent/Display.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import '../../styles/Display.scss';
 
-class Display extends React.Component {
-  render() {
-    return (
-      <div className='display'>
-        <div className='display-items'>
-          <div className='display-block'>块级元素1
-            <span className='display-inline'>行内元素1</span></div>
-          <div className='display-block'>块级元素2</div>
-        </div>
+const Display = () => (
+  <div className='display'>
+    <div className='display-items'>
+      <div className='display-block'>块级元素1
+        <span className='display-inline'>行内元素1</span></div>
+      <div className='display-block'>块级元素2</div>
+    </div>
 
-        <div className='display-items'>
-          <span className='display-inline'>行内元素1</span>
-          <span className='display-inline'>行内元素2</span>
-        </div>
+    <div className='display-items'>
+      <span className='display-inline'>行内元素1</span>
+      <span className='display-inline'>行内元素2</span>
+    </div>
 
-        <div className='display-items'>
-          <div className='display-inline-block'>Inline block</div>
-          <div className='display-inline-block'>Inline block</div>
-          <div className='display-inline-block'>Inline block</div>
-          <div className='display-inline-block'>Inline block</div>
-          <div className='display-inline-block'>Inline block</div>
-          <div className='display-inline-block'>Inline block</div>
-        </div>
-      </div>
-    )
-  }
-}
+    <div className='display-items'>
+      <div className='display-inline-block'>Inline block</div>
+      <div className='display-inline-block'>Inline block</div>
+      <div className='display-inline-block'>Inline block</div>
+      <div className='display-inline-block'>Inline block</div>
+      <div className='display-inline-block'>Inline block</div>
+      <div className='display-inline-block'>Inline block</div>
+    </div>
+  </div>
+);
 
 export default Display;

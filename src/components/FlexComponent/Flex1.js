d66968281db21645cce78a2609d2591c9c1e7419
ruyu You <ryyou@thoughtworks.com>
2017-10-09 17:55:16 +0800

    Refactor: extract common styles for flex1 flex2 component

--- src/components/FlexComponent/Flex1.js
+++ src/components/FlexComponent/Flex1.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../styles/Flex1.scss';
 
 const Flex1 = () => (
-  <div className='flex1'>
+  <div className='flex-container flex1'>
     <div className='case-box left'></div>
     <div className='case-box middle'></div>
     <div className='case-box right'></div>
--- src/components/FlexComponent/Flex2.js
+++ src/components/FlexComponent/Flex2.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../styles/Flex2.scss';
 
 const Flex2 = () => (
-  <div className='flex2'>
+  <div className='flex-container flex2'>
     <div className='case-box'></div>
     <div className='case-box'></div>
     <div className='case-box'></div>

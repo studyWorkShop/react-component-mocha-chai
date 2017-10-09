3956104001af8a02476a9e564ff7df15c20ba60f
ruyu You <ryyou@thoughtworks.com>
2017-10-09 17:39:32 +0800

    Refactor: change Flex2 component to stateless component and delete free code

--- src/components/FlexComponent/Flex2.js
+++ src/components/FlexComponent/Flex2.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import '../../styles/Flex2.scss';
 
-class Flex2 extends React.Component {
-  render() {
-    return (
-      <div className='flex2'>
-        <div className='case-box'></div>
-        <div className='case-box'></div>
-        <div className='case-box'></div>
-      </div>
-    );
-  }
-}
+const Flex2 = () => (
+  <div className='flex2'>
+    <div className='case-box'></div>
+    <div className='case-box'></div>
+    <div className='case-box'></div>
+  </div>
+);
 
 export default Flex2;

c404eb2e824e9b9ca3a9d77c6aa1e3b62609d23c
ruyu You <ryyou@thoughtworks.com>
2017-10-09 17:36:44 +0800

    Refactor: change Flex1 component to stateless component and delete free code

--- src/components/FlexComponent/Flex1.js
+++ src/components/FlexComponent/Flex1.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import '../../styles/Flex1.scss';
 
-class Flex1 extends React.Component {
-  render() {
-    return (
-      <div className='flex1'>
-        <div className='case'>
-          <div className='case-box left'></div>
-          <div className='case-box middle'></div>
-          <div className='case-box right'></div>
-        </div>
-      </div>
-    );
-  }
-}
+const Flex1 = () => (
+  <div className='flex1'>
+    <div className='case-box left'></div>
+    <div className='case-box middle'></div>
+    <div className='case-box right'></div>
+  </div>
+);
 
 export default Flex1;

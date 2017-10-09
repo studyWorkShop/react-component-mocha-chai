82999deb76dbf582c0ae9afa4652fa3b87689694
ruyu You <ryyou@thoughtworks.com>
2017-10-09 16:47:02 +0800

    add case1 for flex1 style

--- src/components/FlexComponent/Flex1.js
+++ src/components/FlexComponent/Flex1.js
@@ -4,8 +4,12 @@ import '../../styles/Flex1.scss';
 class Flex1 extends React.Component {
   render() {
     return (
-      <div>
-        Flex1
+      <div className='flex1'>
+        <div className='case'>
+          <div className='case-box left'></div>
+          <div className='case-box middle'></div>
+          <div className='case-box right'></div>
+        </div>
       </div>
     );
   }

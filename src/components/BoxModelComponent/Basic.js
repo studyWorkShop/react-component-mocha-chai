2e7e70bafacd2db4ee0691e9f67ec262ef9d613a
ruyu You <ryyou@thoughtworks.com>
2017-10-09 17:27:40 +0800

    Refactor: change basic component to stateless component

--- src/components/BoxModelComponent/Basic.js
+++ src/components/BoxModelComponent/Basic.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import '../../styles/BoxModel.scss';
 
-class Basic extends React.Component {
-  render() {
-    return (
-      <div id='box-basic'>
-        <span className='margin-text'>Margin Edge</span>
-        <div className='margin-edge'>
-          <span className='border-text'>border Edge</span>
-          <div className='border-edge'>
-            <span className='padding-text'>padding Edge</span>
-            <div className='padding-edge'>
-              <div className='content-text'>content Edge</div>
-            </div>
-          </div>
+const Basic = () => (
+  <div id='box-basic'>
+    <span className='margin-text'>Margin Edge</span>
+    <div className='margin-edge'>
+      <span className='border-text'>border Edge</span>
+      <div className='border-edge'>
+        <span className='padding-text'>padding Edge</span>
+        <div className='padding-edge'>
+          <div className='content-text'>content Edge</div>
         </div>
       </div>
-    )
-  }
-}
+    </div>
+  </div>
+);
 
 export default Basic;

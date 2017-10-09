c7f34308fcb37d9515d7d4d3bf48930db41ec341
ruyu You <ryyou@thoughtworks.com>
2017-10-09 17:22:33 +0800

    add flex2 style

--- src/components/FlexComponent/Flex2.js
+++ src/components/FlexComponent/Flex2.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import '../../styles/Flex2.scss';
 
 class Flex2 extends React.Component {
   render() {
     return (
-      <div>
-        Flex2
+      <div className='flex2'>
+        <div className='case-box'></div>
+        <div className='case-box'></div>
+        <div className='case-box'></div>
       </div>
     );
   }

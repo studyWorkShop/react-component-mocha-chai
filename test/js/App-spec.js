403a670c21dc608ffacf59070a4fafbb3a2a0852
ruyu You <ryyou@thoughtworks.com>
2017-10-11 11:09:30 +0800

    add App Link test

---
+++ test/js/App-spec.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {expect} from 'chai';
+import {mount} from 'enzyme';
+import {MemoryRouter} from 'react-router';
+
+import App from '../../src/components/App';
+
+describe('App component', () => {
+  describe('App Link', () => {
+    it('App Link children should be 2', () => {
+      const wrapper = mount(<App/>);
+      expect(wrapper.find('ul').children()).to.have.length(2);
+    });
+
+    it('App Link href', () => {
+      const node = document.createElement('div');
+      render((
+        <MemoryRouter>
+          <App/>
+        </MemoryRouter>
+      ), node);
+
+      const href = node.querySelector('a').getAttribute('href');
+      expect(href).to.be.equal('/box-model');
+    });
+  });
+});
